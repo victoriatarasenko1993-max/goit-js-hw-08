6cdf3b69b6e954105348945c48696b848d303956
victoriatarasenko1993-max <victoriatarasenko1993@gmail.com>
2026-04-15 21:58:57 +0300

    hw8js

--- js/gallery.js
+++ js/gallery.js
@@ -59,7 +59,7 @@ description: 'Nature Landscape',
 preview:
  'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
 original:
- https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+ 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
 description: 'Lighthouse Coast Sea',
 },
 ];
